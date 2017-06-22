bb6b4e7b886d146affac1798cfd9acd02693c861
Faith <kaixiaofei@gmail.com>
2017-06-22 10:34:20 +0800

    Initial Commit

---
+++ SmartBPG/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmartBPG
+//
+//  Created by JustFei on 2017/6/22.
+//  Copyright © 2017年 manridy.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SmartBPG/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmartBPG
+//
+//  Created by JustFei on 2017/6/22.
+//  Copyright © 2017年 manridy.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
